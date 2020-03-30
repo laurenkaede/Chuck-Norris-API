82589437ab551b16a274282401ba9a39aba6d1bf
Lauren Moor <laurenkaede@gmail.com>
2020-03-30 18:14:53 +0100

    Updates

---
+++ precache-manifest.95940be5a17543a69e00a393a3917ae3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52366b99bb3c83b1494ae2ee75453b84",
+    "url": "/Chuck-Norris-API/index.html"
+  },
+  {
+    "revision": "bc35e48f4017625a7937",
+    "url": "/Chuck-Norris-API/static/css/main.f115099d.chunk.css"
+  },
+  {
+    "revision": "7a8e9fdbd3f5ee616907",
+    "url": "/Chuck-Norris-API/static/js/2.926f5298.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Chuck-Norris-API/static/js/2.926f5298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc35e48f4017625a7937",
+    "url": "/Chuck-Norris-API/static/js/main.29d4a45d.chunk.js"
+  },
+  {
+    "revision": "a83b5acc7db5fd32c291",
+    "url": "/Chuck-Norris-API/static/js/runtime-main.36e3c2c9.js"
+  },
+  {
+    "revision": "0607b2e8f4132a2809ca6f4693409bda",
+    "url": "/Chuck-Norris-API/static/media/chucknorris.0607b2e8.png"
+  },
+  {
+    "revision": "44a5a282387fb45fd6436db5cf9dbbf4",
+    "url": "/Chuck-Norris-API/static/media/tumbleweed.44a5a282.gif"
+  }
+]);
